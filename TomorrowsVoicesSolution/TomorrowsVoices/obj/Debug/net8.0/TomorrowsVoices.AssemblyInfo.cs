3ed482e79d4964a893e06628f5eb1bb7ebc0aef5
Radshah01 <142700835+Radshah01@users.noreply.github.com>
2025-01-22 19:12:34 -0500

    gitignore

--- TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
+++ TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab3bf835267b584b04883dfe629f6dfc9dcf8a35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e7f29339fb97bca6eaf4d654f53f1a1dc39c34")]
 [assembly: System.Reflection.AssemblyProductAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
