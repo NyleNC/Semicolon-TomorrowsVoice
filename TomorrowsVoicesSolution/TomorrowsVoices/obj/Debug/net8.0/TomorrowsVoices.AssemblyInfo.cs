5ad48bb6632c27122aff3847ef3f5109cb303b7e
NyleNC <nyle.bumanglag@gmail.com>
2025-01-22 22:08:07 -0500

    Updated Views for edit create and details
    made the city a dropdown

--- TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
+++ TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fcab90501d9c764bbea26c16e984194031f48a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ecb53d56ce28f082a6920916063fc2381cdb59")]
 [assembly: System.Reflection.AssemblyProductAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
