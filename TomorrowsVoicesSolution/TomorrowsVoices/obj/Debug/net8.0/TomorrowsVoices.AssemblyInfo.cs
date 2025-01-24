2eb1e7b3ad88de994e4a2ce50356fd9cce77b581
SahilPatel-02 <152944026+SahilPatel-02@users.noreply.github.com>
2025-01-23 23:30:42 -0500

    Excluded some files for git.

--- TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
+++ TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10cf4770e4181e1d107692705ca4b969b8d9815b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2053a66a8fab2ff02d56a29f885e403c300ae830")]
 [assembly: System.Reflection.AssemblyProductAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
