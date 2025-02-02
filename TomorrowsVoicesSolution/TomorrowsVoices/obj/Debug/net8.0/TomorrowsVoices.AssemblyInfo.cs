769e68025497da9421ceb456664ba12466635e1a
Yaseen <mohammadyaseen_2002@outlook.com>
2025-02-02 01:19:02 -0500

    Added AutoFill Fields button on Create Views of Attendance, Director and Singer
    Drop down lists and multi select list are not working with the auto fill buttons so have to do that manually during the demonstration.

--- TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
+++ TomorrowsVoicesSolution/TomorrowsVoices/obj/Debug/net8.0/TomorrowsVoices.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2512fe844c8abc74892494f95c757ef91d9b831f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5c89dffed37024462bfe85cd9bdbedb19c1a211f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TomorrowsVoices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
