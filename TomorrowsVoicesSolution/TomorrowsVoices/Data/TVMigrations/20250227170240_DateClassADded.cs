1883df0e326a7690b777884a526802cd3c72343e
NyleNC <nyle.bumanglag@gmail.com>
2025-02-27 12:17:42 -0500

    Added a popup for the archiving for confirmation of either archiving or unarchiving
    Also added new migrations and database where it adds the new Data Class that Radin did

---
+++ TomorrowsVoicesSolution/TomorrowsVoices/Data/TVMigrations/20250227170240_DateClassADded.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TomorrowsVoices.Data.TVMigrations
+{
+    /// <inheritdoc />
+    public partial class DateClassADded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "VolAttendances",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "Date",
+                table: "Events",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "VolAttendances");
+
+            migrationBuilder.DropColumn(
+                name: "Date",
+                table: "Events");
+        }
+    }
+}
